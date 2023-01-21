98cf5c380cafe928e08e7a55417cd0bb817b177a
Albert Manasov <alikmanasow@gmail.com>
2023-01-21 16:38:45 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import javax.swing.*;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl test = new UserServiceImpl();
+/*
+       //Создаю таблицу
+      test.createUsersTable();
+
+*/
+       //добавляю в таблицу
+       test.saveUser("Альберт", "Манасов", (byte) 25);
+       test.saveUser("Иван", "Иванов", (byte) 21);
+    //чекаю список всех юзеров в таблице
+
+
+
+       /*//удаляю юзера по id
+        test.removeUserById(2);
+        System.out.println(test.getAllUsers());*/
+        test.cleanUsersTable();
+        System.out.println(test.getAllUsers());
+
+
+
+
     }
 }
